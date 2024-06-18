31cbd6175409add9209479df0c70202043379c2e
jhw030306 <154288326+jhw030306@users.noreply.github.com>
2024-06-18 16:19:47 +0000

    30!

--- lesson-31/app.js
+++ lesson-31/app.js
@@ -95,19 +95,16 @@ app.use((req, res, next) => {
  * =====================================================================
  */
 
-// 애플리케이션에 Mongoose 설정
-const mongoose = require("mongoose"), // mongoose를 요청
-  dbName = "aaronkr";
+const mongoose = require("mongoose"); // mongoose를 요청
 
 // 데이터베이스 연결 설정
-mongoose.connect(`mongodb://127.0.0.1:27017/${dbName}`, {
-  useNewUrlParser: true,
+mongoose.connect(
+  "mongodb+srv://jhw030306:1234*@ut-node.pzxaori.mongodb.net/?retryWrites=true&w=majority&appName=UT-Node", {
 });
 
-// 연결되면 메시지를 보냄
 const db = mongoose.connection;
-db.once("open", () => {
-  console.log(`Connected to ${dbName} MongoDB using Mongoose!`);
+db.once("opne", () => {
+  console.log("Connected to MONGODB!!");
 });
 
 /**
@@ -173,5 +170,5 @@ const server = app.listen(app.get("port"), () => {
     // 3000번 포트로 리스닝 설정
     console.log(`Server running at http://localhost:${app.get("port")}`);
   }),
-  io = require("socket.io")(server); // socket.io를 사용하기 위한 서버 객체 설정
+io = require("socket.io")(server); // socket.io를 사용하기 위한 서버 객체 설정
 require("./controllers/chatController")(io); // 채팅 컨트롤러를 요청하고 서버 객체를 전달
--- lesson-32/app.js
+++ lesson-32/app.js
@@ -95,19 +95,17 @@ app.use((req, res, next) => {
  * =====================================================================
  */
 
-// 애플리케이션에 Mongoose 설정
-const mongoose = require("mongoose"), // mongoose를 요청
-  dbName = "aaronkr";
+//애플리케이션에 Mongoose 설정
+const mongoose = require("mongoose"); // mongoose를 요청
 
 // 데이터베이스 연결 설정
-mongoose.connect(`mongodb://127.0.0.1:27017/${dbName}`, {
-  useNewUrlParser: true,
+mongoose.connect(
+  "mongodb+srv://jhw030306:1234*@ut-node.pzxaori.mongodb.net/?retryWrites=true&w=majority&appName=UT-Node", {
 });
 
-// 연결되면 메시지를 보냄
 const db = mongoose.connection;
-db.once("open", () => {
-  console.log(`Connected to ${dbName} MongoDB using Mongoose!`);
+db.once("opne", () => {
+  console.log("Connected to MONGODB!!");
 });
 
 /**
